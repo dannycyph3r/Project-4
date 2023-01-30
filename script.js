58ebfd55be33200c2fd9a43b8ecae1e80ae12051
Dannycyph3r <designmatr1x0@gmail.com>
2023-01-30 15:48:49 -0500

    anim8

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ function removeJump(){
 //game height - character height (200 - 50)
 
 
+// lets create a function called checkDead , to see if they collide
+
+var block = document.getElementById("block");
+function checkDead(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        alert("Game over");
+    }
+}
+
+setInterval(checkDead, 10);
